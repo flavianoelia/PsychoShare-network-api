d3334d687db1a86cb744fec9ceb9bd135715bbec
Coty <constanza.rodriguez@itscordoba.edu.ar>
2025-07-28 20:59:35 -0300

    Create entities system

---
+++ entity_library/system/Person.cs
@@ -0,0 +1,22 @@
+public class Person
+{
+    private int idPerson;
+    private string name = "";
+    private string lastName = "";
+
+    public int IdPerson
+    {
+        get { return idPerson; }
+        set { idPerson = value; }
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+}    
---
+++ entity_library/system/Role.cs
@@ -0,0 +1,16 @@
+public class Role 
+{
+    private int Idrole;
+    private string roleName = "";
+
+    public int IdRole
+    {
+        get { return Idrole; }
+        set { Idrole = value; }
+    }
+    public string RoleName
+    {
+        get { return roleName; }
+        set { roleName = value; }
+    }
+}   
