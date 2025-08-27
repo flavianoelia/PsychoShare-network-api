cd30b0e4fb80e58f26b07193bd21a962f0a37629
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-08-27 19:33:46 -0300

    Add file and image upload endpoints to PostController

--- psychoshare_api/Controllers/PostController.cs
+++ psychoshare_api/Controllers/PostController.cs
@@ -19,6 +19,30 @@ public class PostController : ControllerBase
         // TODO: Create new post
     }
 
+        [HttpPost("{id}/cover-image")]
+        public void UploadCoverImage(int id, IFormFile image)
+        {
+            // Upload cover image for post
+        }
+
+        [HttpPost("{id}/pdf")]
+        public void UploadPdf(int id, IFormFile pdf)
+        {
+            // Upload PDF for post
+        }
+
+        [HttpGet("{id}/cover-image")]
+        public void GetCoverImage(int id)
+        {
+            // Get cover image for post
+        }
+
+        [HttpGet("{id}/pdf")]
+        public void GetPdf(int id)
+        {
+            // Get PDF for post
+        }
+
     [HttpGet("{id}")]
     public void ViewPost(int id)
     {
