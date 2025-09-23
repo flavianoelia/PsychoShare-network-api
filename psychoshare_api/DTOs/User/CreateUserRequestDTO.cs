ce301414632de354939e1a6eb3b50a08aa681c34
Daniel <daniel.nicolas.llanes@gmail.com>
2025-09-23 20:10:44 -0300

    created dto for the reception of user form the front

---
+++ psychoshare_api/DTOs/User/CreateUserRequestDTO.cs
@@ -0,0 +1,10 @@
+namespace psychoshare_api.DTOs.User;
+
+public class CreateUserRequestDTO
+{
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}
