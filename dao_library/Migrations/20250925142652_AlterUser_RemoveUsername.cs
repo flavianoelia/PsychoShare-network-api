5998f8faa4e77742a9ca43cbc9fc939275e0b3a1
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-09-25 19:19:58 -0300

    chore: apply migration to update User entity and remove Username

---
+++ dao_library/Migrations/20250925142652_AlterUser_RemoveUsername.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dao_library.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterUser_RemoveUsername : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
