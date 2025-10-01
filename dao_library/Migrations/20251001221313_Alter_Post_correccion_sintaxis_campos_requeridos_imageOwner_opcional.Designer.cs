dd5fcffc96affdd9eec29871b422d438949aaf28
Daniel <daniel.nicolas.llanes@gmail.com>
2025-10-01 19:18:57 -0300

    Modified the pos table, allowing to receive the user's id, first name, last name, and image

--- dao_library/Migrations/20251001221313_Alter_Post_correccion_sintaxis_campos_requeridos_imageOwner_opcional.Designer.cs
+++ dao_library/Migrations/20251001221313_Alter_Post_correccion_sintaxis_campos_requeridos_imageOwner_opcional.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dao_library.Contexts;
 
@@ -10,9 +11,11 @@ using dao_library.Contexts;
 namespace dao_library.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251001221313_Alter_Post_correccion_sintaxis_campos_requeridos_imageOwner_opcional")]
+    partial class Alter_Post_correccion_sintaxis_campos_requeridos_imageOwner_opcional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
