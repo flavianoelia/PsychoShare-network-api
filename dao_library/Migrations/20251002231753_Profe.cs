7f72926538b8c56c20be40bfdcd1731854fd4f92
fpiedrasanta <fede.piedrasanta@gmail.com>
2025-10-02 21:17:00 -0300

    profe

---
+++ dao_library/Migrations/20251002231753_Profe.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dao_library.Migrations
+{
+    /// <inheritdoc />
+    public partial class Profe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdPerson",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "IdPerson",
+                table: "Users",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
---
+++ dao_library/Migrations/20251002232302_Profe2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dao_library.Migrations
+{
+    /// <inheritdoc />
+    public partial class Profe2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
