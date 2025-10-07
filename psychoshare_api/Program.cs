42f5258381b6fcf5915f66b560c5cb273637ceea
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-10-07 15:03:57 -0300

    fix: remove duplicate DbContext configurations in Program.cs

--- psychoshare_api/Program.cs
+++ psychoshare_api/Program.cs
@@ -32,19 +32,7 @@ builder.Services.AddCors(options =>
         });
 });
 
-
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(option =>
-    option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
 builder.Services.AddScoped<DAOFactory, EFDAOFactory>();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options
-        .UseLazyLoadingProxies()
-        .UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
 var app = builder.Build();
 
