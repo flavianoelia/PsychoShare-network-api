b28865b44715a40b0065bf97e82590ffd45e87b5
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-10-09 00:20:38 -0300

    chore: remove temporary SQL test files

--- insert_test_post.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Posts (Id, UserId, NameOwner, LastnameOwner, Title, Description, Authorship, Resume, PdfId, ImageId, ImgOwnerId) 
-VALUES (1, 1, 'Flavia', 'Perin', 'Mi primer post de prueba', 'Este es un post de prueba para demonstrar el sistema de likes. Contiene información interesante sobre psicología.', 'Flavia Perin', 'Post de prueba para el sistema de likes', NULL, NULL, 1);
-
-SELECT * FROM Posts WHERE Id = 1;
-SELECT * FROM Persons WHERE Id = 1 AND PersonType = 'User';
