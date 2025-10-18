3fcc8b3fe1434a62f80427da210cfbb242305db6
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-10-18 17:26:07 -0300

    feat: DTOs for Post CRUD operations ready

---
+++ psychoshare_api/DTOs/Post/EditPostDto.cs
@@ -0,0 +1,12 @@
+namespace psychoshare_api.DTOs.Post;
+
+public class EditPostDto
+{
+    public long Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Authorship { get; set; }
+    public string? Resume { get; set; }
+    public string? Image { get; set; }
+    public string? Pdf { get; set; }
+}
