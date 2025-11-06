2c794d780b79fe379468d2b596e2b18efb151b57
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-11-06 15:11:42 -0300

    feat: add pagination support to PostDAO with GetAllPostsPaginated method

--- dao_library/interfaces/social_media_core/DAOPost.cs
+++ dao_library/interfaces/social_media_core/DAOPost.cs
@@ -4,6 +4,7 @@ public interface DAOPost
     List<Post> GetPostFromUser(long IdUser);
     List<Post> GetAllPosts();
     (List<Post> Posts, int TotalCount) GetAllPostsPaginated(int page, int size, string? searchTerm = null);
+    (List<Post> Posts, int TotalCount) GetFeedPosts(long currentUserId, int page, int size, string? searchTerm = null);
     void Save(Post post);
     void UpdatePost(long IdPost);
     void Delete(long IdPost);
