687c781cb5a99a15aa22dcdc593b3206a8c1463b
Vero <veronica.ramirez@itscordoba.edu.ar>
2025-11-21 11:23:28 -0300

    I re-fatorizo, the only method to create or update an avatar.

--- psychoshare_api/Services/Interfaces/IAvatarService.cs
+++ psychoshare_api/Services/Interfaces/IAvatarService.cs
@@ -5,8 +5,7 @@ namespace psychoshare_api.Services.Interfaces;
 
 public interface IAvatarService
 {
-    public AvatarResponseDTO UploadAvatar(long userId, IFormFile file);
-    public AvatarResponseDTO UpdateAvatar(long userId, UploadAvatarDto dto);
+    public AvatarResponseDTO UpsertAvatar(long userId, IFormFile file);
     public AvatarResponseDTO? GetUserAvatar(long userId);
     public void DeleteAvatar(long userId);
 }
