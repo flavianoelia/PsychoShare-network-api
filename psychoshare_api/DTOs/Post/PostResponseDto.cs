f7ab89ef32e840dac92625c64d2b986828a1e86d
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-11-24 12:24:25 -0300

    Add AvatarUrl to PostResponseDto and update avatar images

--- psychoshare_api/DTOs/Post/PostResponseDto.cs
+++ psychoshare_api/DTOs/Post/PostResponseDto.cs
@@ -6,6 +6,7 @@ public class PostResponseDto
     public long UserId { get; set; }
     public string NameOwner { get; set; } = "";
     public string LastnameOwner { get; set; } = "";
+    public string? AvatarUrl { get; set; }
     public string Title { get; set; } = "";
     public string Description { get; set; } = "";
     public string Authorship { get; set; } = "";
